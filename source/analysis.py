910ca1d408067225aa2bc177f983341d4806cd83
Justin <dengitzjg@gmail.com>
2015-12-07 01:04:23 -0500

    Added a plotting function

--- source/analysis.py
+++ source/analysis.py
@@ -1,8 +1,11 @@
 import json
 from pprint import pprint
+import matplotlib.pyplot as plt
+import numpy as np
 
 with open('Market List/Demon Eater.txt') as data_file:
 	data = json.load(data_file);
+#data['prices'][DATE][PRICE]
 
 def getMovingAVG(movingAVGLength):
 	movingAVG = []
@@ -13,20 +16,11 @@ def getMovingAVG(movingAVGLength):
 		avg = 0.0
 		for y in range(x-movingAVGLength, x):
 			avg = listOfPoints[y] + avg
-		movingAVG.append(float(avg/movingAVGLength))
-	print movingAVG
+		point = [x,float(avg/movingAVGLength)]
+		movingAVG.append(point)
+	return movingAVG
 
-#usage: data['prices'][DATE][PRICE]
 
-def generate_population(MA_length):
-	array = []
-	population = []
-	current = 2**MA_length - 1
-	while current > 0:
-		array = [int(x) for x in str(bin(current))[2:].zfill(MA_length)]
-		population.append(array);
-		current -= 1
-	return population
-
-getMovingAVG(500)
-print generate_population(3)
+a = getMovingAVG(3)
+plt.plot(*zip(*a))
+plt.show()
